61fc12f8a66a68a0a092acb3cbeec00f052131bc
Md. Nahidul Islam <nahidulss@gmail.com>
2021-09-09 12:33:43 +0600

    linked list reverse recursive

--- leetcode/reverselinkedlist206.py
+++ leetcode/reverselinkedlist206.py
@@ -37,5 +37,44 @@ class Solution:
             run=temp
         return prev
 
+
+#reverse linkid list  recursive way
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #rucursive
+       # print(head.val)
+#         #newhead=None
+#         if head is None:
+#             return head
+#         if head.next is None:
+#             self.head=head
+#            # print(newhead.val)
+#             return head
+        
+#         self.reverseList(head.next)
+#         #print(head.val)
+#         prev=head.next
+#         #print(prev.val)
+#         prev.next=head
+#         #print(prev.next.val)
+#         head.next=None
+#         return self.head
+        if not head or not head.next :
+            return head
+        reverse=self.reverseList(head.next)
+        print(reverse)
+        print(reverse.val)
+        head.next.next=head
+        print("linking head" ,head.next.val)
+        print(" head" ,head.val)
+        head.next=None   
+        return reverse
+
     
             
